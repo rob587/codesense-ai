593662a97d4978409cc84fd7315378e77984649a
rob587 <robertocamm02@yahoo.com>
2026-06-25 12:50:56 +0200

    add review router and integrate with express server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+//importare routes
+import reviewRouter from "./routes/review.js";
 
 dotenv.config();
 
@@ -9,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", reviewRouter);
 
 app.listen(PORT, () => {
   console.log(`Server aperto nella porta: ${PORT}`);
